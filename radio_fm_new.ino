d7c69e7ff6fd73d1acb1f0831862a9e8e1cbb358
Gean Marcos Geronymo <gean.geronymo@gmail.com>
2017-10-16 15:50:09 -0200

    versao inicial - comunicacao via seral

--- radio_fm_new.ino
+++ radio_fm_new.ino
@@ -7,7 +7,12 @@
 U8GLIB_PCD8544 u8g(8, 9, 11, 10, 12);		//CLK=8, DIN=9, CE=11, DC=10, RST=12
 
 int channel;
+int channel_min = 875;
+int channel_max = 1080;
+
 char frequencyString[10];
+int inByte;
+int level;
 
 /************************************************** ************************************************** */
 byte frequencyH = 0;
@@ -18,10 +23,14 @@ double frequency = 0;
 
 void draw(void) {
   // graphic commands to redraw the complete screen should be placed here
-  u8g.setFont(u8g_font_profont15);  
-  u8g.setPrintPos(20, 20); 
+  //u8g.setFont(u8g_font_profont15);
+  u8g.setFont(u8g_font_courB18);
+  u8g.setPrintPos(15, 25); 
   u8g.print(frequency,1);
-  //u8g.print(" MHz");
+  
+  u8g.setFont(u8g_font_profont12);
+  u8g.setPrintPos(20, 40);
+  u8g.print(level);
 }
 
 /************************************************** ************************************************** */
@@ -43,6 +52,25 @@ void setFrequency()
 }
 /************************************************** ************************************************** */
 
+/************************************************** ************************************************** */
+void getSignalLevel()
+{
+  unsigned char buffer[5];
+  
+  Wire.requestFrom(0x60,5); //reading TEA5767
+
+  if (Wire.available()) 
+  {
+    for (int i=0; i<5; i++) {
+      buffer[i]= Wire.read();
+    }
+
+    level = buffer[3]>>4;
+  }
+  
+}
+/************************************************** ************************************************** */
+
 void setup(void) {
   // flip screen, if required
   // u8g.setRot180();
@@ -52,9 +80,32 @@ void setup(void) {
   channel = 931;
   setFrequency();
   
+  Serial.begin(9600);
+  
 }
 
 void loop(void) {
+  // serial + ou -
+  if (Serial.available()>0) {
+    inByte = Serial.read();
+      if (inByte == '+') {
+        if (channel < channel_max) {
+          channel++;
+          setFrequency();
+        }
+    }
+    if (inByte == '-') {
+        if (channel > channel_min) {
+          channel--;
+          setFrequency();
+        }
+    } 
+    
+  }
+  
+  delay(50);
+  
+  getSignalLevel();
   
   // picture loop
   u8g.firstPage();  
@@ -63,5 +114,5 @@ void loop(void) {
   } while( u8g.nextPage() );
   
   // rebuild the picture after some delay
-  delay(50);
+  delay(500);
 }
